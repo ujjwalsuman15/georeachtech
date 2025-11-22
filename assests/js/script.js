b6510c1b4a3e16fbd7305adc05ad44e02102cb5b
ujjwalsuman15 <chaurasiyaujjwalsuman2004@gmail.com>
2025-11-22 17:17:59 +0530

    written review section added

--- assests/js/script.js
+++ assests/js/script.js
@@ -28,6 +28,7 @@ new Swiper(".placedStudentSwiper", {
     },
 });
 //placedStudent end 
+
 //Student Review start 
 new Swiper('.slider-wrapper', {
     loop: true,
@@ -67,3 +68,41 @@ new Swiper('.slider-wrapper', {
     }
 });
 // Student Review start end
+
+
+//WRITTEN-REVIEW js section start 
+
+var swiper = new Swiper(".myreviewSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    // loopFillGroupWithBlank: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 90000,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
+
+// WRITTEN-REVIEW js section start end
